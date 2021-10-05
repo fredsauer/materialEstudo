b0062356307064332e47770f0df13392b6525f4d
fredsauer <90940015+fredsauer@users.noreply.github.com>
2021-10-05 19:57:55 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -8,6 +8,7 @@ namespace Api.LojaTav2021.Model
     public class Cliente
     {
         public int idCliente { get; set; }
+        public string paisOrigem { get; set; }
         public string endereco { get; set; }
         public int cep { get; set; }
     }
