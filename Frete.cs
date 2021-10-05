0cc5dd61e8975980cda631011197e476760e6465
fredsauer <90940015+fredsauer@users.noreply.github.com>
2021-10-05 19:43:53 -0300

    Create Frete.cs

---
+++ Frete.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.LojaTav2021.Model
+{
+    public class Frete
+    {
+        public int idFrete { get; set; }
+        public double valorFrete { get; set; }
+        public int cep { get; set; }
+    }
+}
